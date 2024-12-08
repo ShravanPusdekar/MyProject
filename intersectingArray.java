ffe7dfb1f0c69da5cde37100cf2b6b1311677ed0
Shravan Pusdekar <shravanpusdekar16@gmail.com>
2024-12-09 01:14:02 +0530

    Intial commit

---
+++ intersectingArray.java
@@ -0,0 +1,51 @@
+import java.util.*;
+;
+
+public class intersectingArray {
+    static Scanner sc=new Scanner(System.in);
+    static int[]arr1,arr2;
+    static int n,m;
+    static HashSet<Integer> set = new HashSet<>();
+    static ArrayList<Integer> result=new ArrayList<>();
+
+    public static void getData(){
+        System.out.println("Enter the number of elements of first array");
+         n= sc.nextInt();
+        arr1=new int[n]; // assigning the length of arr1
+        System.out.println("Enter first array");
+        for (int i = 0; i < n; i++) {
+            arr1[i]= sc.nextInt(); // accepting first array
+        }
+         System.out.println("Enter the number of elements of second array");
+         m= sc.nextInt();
+        arr2=new int[m]; // assigning the length of arr2
+        System.out.println("Enter second array");
+        for (int i = 0; i < m; i++) {
+            arr2[i]= sc.nextInt();   // accepting second array
+        }
+    }
+
+    public static void checkIntersection(){
+
+        for (int i = 0; i < n; i++) {
+            set.add(arr1[i]);
+        }
+        for(int i:arr2){
+          if(set.contains(i)){
+              result.add(i);
+              set.remove(i);
+          }
+        }
+
+    }
+
+    public static void printData(){
+        System.out.println("The Intersection of two array is:"+result);
+    }
+
+    public static void main(String[] args) {
+        getData();
+        checkIntersection();
+        printData();
+    }
+}
--- rotateArray.java
+++ rotateArray.java
@@ -1,23 +1,64 @@
 import java.util.Scanner;
 
 public class rotateArray {
-    public static void main(String[] args) {
-        int elements,steps;
+    static int n,stp,side;
+    static int []arr1,arr3;
+
+    public static void getData()  {
+
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number of elements");
-        elements=sc.nextInt();
-        int[] arr1=new int[elements];
-        int[] arr2 =new int[elements];
-        int[] arr3 =new int[elements];
-        for (int i = 0; i <elements ; i++) {
-            arr3[i]= sc.nextInt();
+        n=sc.nextInt();
+        arr1=new int[n];
+        arr3 =new int[n];
+
+        System.out.println("Enter the array");
+        for (int i = 0; i <n ; i++) {
+            arr1[i]= sc.nextInt(); // accepting arr1
         }
-        for (int i = 0; i < elements; i++) {
-            arr2[i]=arr1[elements-i-1];
-        }
+
+
+        System.out.println("Which side would you like to rotate\n" +
+                "1 for Left\n" +
+                "2 for Right");
+        side = sc.nextInt();
+
         System.out.println("Enter the number of steps");
-        steps= sc.nextInt();
-
-
+        stp=sc.nextInt();
     }
+    public static void left(){
+        for (int i = 0; i <n-stp ; i++) {
+            arr3[i]=arr1[stp+i]; // putting (elements - steps ) items in arr3
+        }
+        for (int i = 0; i < stp; i++) {
+            arr3[n-stp+i]=arr1[i];// putting stp items in arr3
+        }
+        System.out.println("The Resultant rotated array is ");
+        for (int i = 0; i <n ; i++) {
+            System.out.println(arr3[i]); // displaying resultant array i.e. arr3
+        }
+    }
+    public static void right(){
+        for (int i = 0; i < stp; i++) {
+            arr3[i]=arr1[n-stp+i];// putting  steps (stp) items in arr3
+        }
+        for (int i = 0; i < n - stp; i++) {
+            arr3[stp+i]=arr1[i]; //putting remaining (elements-stp) items from arr1 to arr3
+        }
+        System.out.println("The Resultant rotated array is ");
+        for (int i = 0; i <n ; i++) {
+            System.out.println(arr3[i]); // displaying resultant array i.e. arr3
+        }
+    }
+    public static void main(String[] args) {
+        getData();
+        if (side==1) { // for rotating left
+            left();
+        } else if (side==2) {  // for rotating right
+            right();
+        } else {
+            System.out.println("Wrong input");
+        }
+    }
+
 }
